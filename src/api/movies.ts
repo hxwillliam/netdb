df03d089b0c18acd3ac8f250a7657683dc6960c8
hxwilliam <159127557+hxwillliam@users.noreply.github.com>
2025-02-05 15:00:31 +0100

    turned keys.ts to .env and updated imports

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,16 +1,15 @@
 import { ContentType } from '../types/ContentType';
 import { Response } from '../types/ResponseType';
-import { API_URL, IMG_BASE_URL } from './keys';
 
 export const getMovies = async (page: number = 1): Promise<ContentType[]> => {
     try {
-        const res = await fetch(`${API_URL}&page=${page}`);
+        const res = await fetch(`${import.meta.env.VITE_API_URL}&page=${page}`);
         const data: Response = await res.json();
         return data.results.map((movie) => ({
             id: movie.id,
             title: movie.title || movie.name || 'Untitled', 
             desc: movie.overview,
-            image: `${IMG_BASE_URL}${movie.poster_path}`,
+            image: `${import.meta.env.VITE_IMG_BASE_URL}${movie.poster_path}`,
             media_type: movie.media_type || 'movie'
         }));
     } catch (error) {
--- src/api/person.ts
+++ src/api/person.ts
@@ -1,16 +1,15 @@
 import { ContentType } from '../types/ContentType';
 import { Response } from '../types/ResponseType';
-import { API_KEY, IMG_BASE_URL, imagePlaceholder } from './keys';
 
 export const getPeople = async (page: number = 1): Promise<ContentType[]> => {
     try {
-        const res = await fetch(`https://api.themoviedb.org/3/trending/person/day?api_key=${API_KEY}&page=${page}`);
+        const res = await fetch(`https://api.themoviedb.org/3/trending/person/day?api_key=${import.meta.env.VITE_API_KEY}&page=${page}`);
         const data: Response = await res.json();
         return data.results.map((person) => ({
             id: person.id,
             title: person.name || 'Unknown',
             desc: '',
-            image: person.profile_path ? `${IMG_BASE_URL}${person.profile_path}` : imagePlaceholder,
+            image: person.profile_path ? `${import.meta.env.VITE_IMG_BASE_URL}${person.profile_path}` : import.meta.env.VITE_IMAGE_PLACEHOLDER,
             media_type: 'person'
         }));
     } catch (error) {
